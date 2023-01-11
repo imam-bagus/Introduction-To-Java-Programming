992f1e3cbec3f31cfdacd86f511418c23d420886
Imam Bagus Nugroho <imambagus9864@gmail.com>
2023-01-11 11:28:55 +0700

    Complete Find The number of years

---
+++ CH2-EXERCISES/FindTheNumberOfYears.java
@@ -0,0 +1,25 @@
+/** Write a program that prompts the user to enter the minutes (e.g., 1 billion), and displays the number of years and remaining days for the minutes. For simplicity, assume that a year has 365 days. */
+
+import java.util.Scanner;
+
+public class FindTheNumbersOfYears{
+    public static void main(String[] args){
+        Scanner input = new Scanner(System.in);
+        long totalMinutesOneYear;
+        long totalMinutesOneDay;
+        long minutes;
+        long years;
+        long days;
+
+        System.out.print("Enter the number of minutes:");
+        minutes = input.nextLong();
+        totalMinutesOneDay = 60 * 24;
+        totalMinutesOneYear = totalMinutesOneDay * 365;
+
+        years = minutes / totalMinutesOneYear;
+        days =  (minutes % totalMinutesOneYear) / totalMinutesOneDay;
+
+        System.out.print(minutes + " minutes is approximately " + years + " Years and " + days + " Days");
+
+    }
+}
