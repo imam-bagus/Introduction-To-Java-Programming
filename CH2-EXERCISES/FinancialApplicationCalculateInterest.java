6cb777b97110188e24181650834d19aa8f1a6af3
Imam Bagus Nugroho <imambagus9864@gmail.com>
2023-01-12 12:39:27 +0700

    completed chapter 2

---
+++ CH2-EXERCISES/FinancialApplicationCalculateInterest.java
@@ -0,0 +1,26 @@
+/*
+If you know the balance and the annual percentage interest rate, you can compute the interest on the next monthly payment using the following formula:
+
+interest = balance * (annualInterestRate/1200)
+
+Write a program that reads the balance and the annual percentage interest rate and displays the interest for the next month.
+*/
+
+import java.util.Scanner;
+
+public class FinancialApplicationCalculateInterest{
+    public static void main(String[] args){
+        Scanner input = new Scanner(System.in);
+        float balance;
+        float annualInterestRate;
+        float interest;
+
+        System.out.print("Enter balance and interest rate (e.g., 3 for 3% :");
+        balance = input.nextFloat();
+        annualInterestRate = input.nextFloat();
+
+        interest = balance * (annualInterestRate/1200);
+
+        System.out.print("The interest is " + interest);
+    }
+}
---
+++ CH2-EXERCISES/HealthApplicationComputeBMI.java
@@ -0,0 +1,26 @@
+/*
+Body Mass Index (BMI) is a measure of health on weight. 
+It can be calculated by taking your weight in kilograms and dividing, by the square of your height in meters. 
+Write a program that prompts the user to enter a weight in pounds and height in inches and displays the BMI.
+Note one pound is 0.45359237 kilograms and one inch is 0.0254 meters
+*/
+
+import java.util.Scanner;
+
+public class HealthApplicationComputeBMI {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        double heightInches;
+        double weightPounds;
+        double BMI;
+
+        System.out.print("Enter weight in pounds: ");
+        weightPounds = input.nextDouble();
+        System.out.print("Enter height in inches: ");
+        heightInches = input.nextDouble();
+
+        BMI = weightPounds * 0.45359237 / (heightInches *  0.0254 * heightInches * 0.0254);
+
+        System.out.print("BMI is " + BMI);
+    }
+}
